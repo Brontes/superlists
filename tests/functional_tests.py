c5d93020b64dbe92538928dd2549eebd6b05aa9e
Klemen Pukl <klemen.pukl@velis.si>
2018-07-27 09:53:56 +0200

    adding to list

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -11,6 +11,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+        pass
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -37,11 +38,21 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-                        "New to-do item did not appear in table")
+        # self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+        #                 f"New to-do item did not appear in table. Contents were:\n{table.text}")
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
         self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
